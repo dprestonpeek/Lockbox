668f53110cefbe152ce70b5321205a243ff02768
Preston Peek <36671440+dprestonpeek@users.noreply.github.com>
2025-05-30 23:28:47 -0400

    Empty project, input started

---
+++ Lockbox/Intermediate/Build/Win64/x64/ShaderCompileWorker/Development/PostBuild-1.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Copying C:\Users\dpres\Documents\Projects\UnrealAgain\Engine\Binaries\Win64\ShaderCompileWorker.exe to C:\Users\dpres\Documents\Projects\UnrealAgain\Engine\Binaries\Win64\XGEControlWorker.exe
+copy /Y /B "C:\Users\dpres\Documents\Projects\UnrealAgain\Engine\Binaries\Win64\ShaderCompileWorker.exe" /B "C:\Users\dpres\Documents\Projects\UnrealAgain\Engine\Binaries\Win64\XGEControlWorker.exe" >nul:
---
+++ Lockbox/Source/Lockbox/Lockbox.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Lockbox.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Lockbox, "Lockbox" );
