d4b142649e8f9f8abec7adef8662e10f8bfc9608
Preston Peek <36671440+dprestonpeek@users.noreply.github.com>
2025-05-31 23:54:54 -0400

    Added puzzle controller c++ class

---
+++ Lockbox/Source/Lockbox/Private/PuzzleController.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuzzleController.h"
+
+
+// Sets default values
+APuzzleController::APuzzleController()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APuzzleController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APuzzleController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APuzzleController::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
